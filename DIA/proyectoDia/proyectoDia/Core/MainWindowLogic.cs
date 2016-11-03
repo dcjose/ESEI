1a78bc1256b8386f9f8238a76c3faa89f4d7598e
dcjose <dcjose@esei.uvigo.es>
2016-11-03 16:21:28 +0100

    Dia files

---
+++ DIA/proyectoDia/proyectoDia/Core/MainWindowLogic.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace proyectoDia
+{
+	public partial class MainWindow
+	{
+		public MainWindow() : base(Gtk.WindowType.Toplevel)
+		{
+			this.Build();
+		}
+
+		private void OnInit()
+		{
+			ImportXml();
+			ListToModel();
+		}
+
+		private void OnClose()
+		{
+			ExportXml();
+			Gtk.Application.Quit();
+		}
+
+
+		private void OnCreate()
+		{
+			short peso = Convert.ToInt16(this.entryPeso.Text);
+			short cadera = Convert.ToInt16(this.entryCadera.Text);
+			Medida me = new Medida(peso, cadera);
+			ListMedidas.Add(me);
+			modelMedidas.AppendValues(me.Peso.ToString(), me.Cadera.ToString(), me.Fecha.ToString());
+		}
+
+
+
+		private void ImportXml() 
+		{	//XML TO LIST
+			ListMedidas = new List<Medida>();
+			var root = XElement.Load("medidas.xml");
+			var childs = from i in root.Elements("Medida") select i.Attributes();
+
+			foreach (var elem in childs)
+			{
+				var peso = Convert.ToInt16(elem.ElementAt(0).Value);
+				var cadera = Convert.ToInt16(elem.ElementAt(1).Value);
+				var fecha = Convert.ToDateTime(elem.ElementAt(2).Value);
+				ListMedidas.Add(new Medida(peso,cadera,fecha));
+			}
+		}
+
+		public void ExportXml()
+		{
+			var xml = new XElement("Medidas",
+					from me in ListMedidas
+						select new XElement("Medida",
+							new XAttribute("Peso", me.Peso),
+							new XAttribute("Cadera", me.Cadera),
+							new XAttribute("Fecha", me.Fecha)
+					));
+			
+
+			xml.Save("medidas.xml");
+		}
+
+
+		private void ListToModel()
+		{
+			foreach (var i in ListMedidas)
+			{
+				modelMedidas.AppendValues(i.Peso.ToString(), i.Cadera.ToString(), i.Fecha.ToString());
+			}
+		}
+
+		private void pesoNameCell_Edited(object o, Gtk.EditedArgs e)
+		{
+			var cosa = new Gtk.TreePath(e.Path);
+			var i = cosa.Indices[0];
+			ListMedidas[i].Peso = Convert.ToInt16(e.NewText);
+		}
+
+		private void caderaNameCell_Edited(object o, Gtk.EditedArgs e)
+		{
+			Gtk.TreeIter iter;
+			if (modelMedidas.GetIter(out iter, new Gtk.TreePath(e.Path)))
+			{
+				modelMedidas.SetValue(iter, 1, e.NewText);
+			}
+		}
+
+
+		private Gtk.ListStore modelMedidas;
+		private List<Medida> ListMedidas;
+
+	}
+}
+
+
